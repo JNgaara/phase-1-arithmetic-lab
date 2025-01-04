5b1e217638749fefd636070fbc0ea1869757095f
Joshua Ngaara <joshua.ngaara@student.moringaschool.com>
2025-01-04 03:22:58 +0200

    arithmetic try A

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let num3 =24;
+let num4 =5;
+var multiply = num1* num2;
+
+var random = Math.floor(Math.random()*10) +1;
+
+var mod = num3 %num4;
+
+let max = Math.max(12,7,2,20,10,14);
+// random
+// 1) generates a random integer greater than 0
+// mod
+// 2) is an equation that calculates a remainder that is equal to 4
+// max
+// 3) will return 20 as the highest number in the set
